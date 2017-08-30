eb8fde86b46736cf2ca32ddf63b1c26ef4f03b31
Matthias Wagler <wagler@intuity.de>
2017-08-30 15:00:55 +0200

    Fix export.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import Brand from './components/Brand';
 import Button from './components/Button';
 import Icon from './components/Icon';
 import setup from './setup';
-import Symbols from './components/Symbols';
 import View from './components/View';
 
-export { Brand, Button, Icon, setup, Symbols, View };
+export { Brand, Button, Icon, setup, View };
